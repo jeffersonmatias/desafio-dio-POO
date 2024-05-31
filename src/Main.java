3dfbc79466e43a198c2bf10ba49864a2f9bcb8dd
jeffersonmatias <138624962+jeffersonmatias@users.noreply.github.com>
2024-05-31 11:20:57 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria Java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+     /*   System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+     */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("Jefferson Carvalho");
+        dev.inscreverBootcamp(bootcamp);
+
+
+        System.out.println("Conteúdos Inscritos: " + dev.getConteudosInscritos());
+        dev.progredir();
+        dev.progredir();
+        System.out.println("Conteúdos Inscritos: " + dev.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos: " + dev.getConteudosConcluidos());
+        System.out.println("XP: " + dev.calcularTotalXp());
+
+    }
+}
